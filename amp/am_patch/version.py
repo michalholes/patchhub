afa48e46be2f0ee2900e4270f3775032d90bc241
Michal Holes <michal@holes.sk>
2026-03-16 10:22:10 +0100

    cleanup after failed porting

--- amp/am_patch/version.py
+++
@@ -1,3 +0,0 @@
-# NOTE: Any change that alters runner behavior MUST bump RUNNER_VERSION and MUST update
-# the runner specification under amp/ (e.g., amp/am_patch_specification.md).
-RUNNER_VERSION = "4.4.8"
